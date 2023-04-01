d868345d6bce4371c774d88234ceb5733e1a9114
duerme <realwooski@protonmail.com>
2023-04-01 15:20:27 -0400

    Modified arp request function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# This is a place holder
+# This is a place holder test
--- maid.py
+++ maid.py
@@ -5,8 +5,12 @@ def arp_request(ip_address):
     arp_packet = scapy.ARP(pdst=ip_address)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     full_packet = broadcast / arp_packet
-    answered, unanswered = scapy.srp(full_packet, timeout=1, iface="Wi-Fi 3", verbose=False)
-    return answered.summary()
+    answered = scapy.srp(full_packet, timeout=1, iface="Wi-Fi", verbose=False)[0]
+    
+    for host in answered:
+        print(host[1].psrc)
+        print(host[1].hwsrc)
+        print("--------------------------------------------------------------")
 
 def device_info(host):
     try:
@@ -15,6 +19,6 @@ def device_info(host):
     except socket.herror as e:
         print(f"Could not resolve the hostname for {host}. Error message: {e}")
 
-arp_request("10.0.0.1/24")
-hostname = device_info("10.0.0.32")
+arp_request("192.168.1.1/24")
+# hostname = device_info("10.0.0.32")
 
