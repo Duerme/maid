ebe93cbeae3ee925f54c4543a66bf76b6def29fe
Duerme <Realwooski@protonmail.com>
2023-04-01 15:57:37 -0400

    Ignoring pycache

--- maid.py
+++ maid.py
@@ -1,11 +1,12 @@
 import scapy.all as scapy
 import socket
+import config
 
 def arp_request(ip_address):
     arp_packet = scapy.ARP(pdst=ip_address)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     full_packet = broadcast / arp_packet
-    answered = scapy.srp(full_packet, timeout=1, iface="Wi-Fi", verbose=False)[0]
+    answered = scapy.srp(full_packet, timeout=1, iface=config.ntwrk_adapter, verbose=False)[0]
     
     for host in answered:
         print(host[1].psrc)
@@ -19,6 +20,6 @@ def device_info(host):
     except socket.herror as e:
         print(f"Could not resolve the hostname for {host}. Error message: {e}")
 
-arp_request("192.168.1.1/24")
+arp_request(config.ip_subnet)
 # hostname = device_info("10.0.0.32")
 
