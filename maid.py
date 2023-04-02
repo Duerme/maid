1e669725adcff9e177d4747b358c2a508bb6b6a0
Duerme <Realwooski@protonmail.com>
2023-04-01 23:20:24 -0400

    Added vendor identification

--- maid.py
+++ maid.py
@@ -2,16 +2,31 @@ import scapy.all as scapy
 import socket
 import config
 
+def collect_vendor_mac():
+    vendor_file = open("mac-vendor.txt", encoding="utf-8")
+    data = vendor_file.read()
+    data_to_list = data.split("\n")
+    mac_identifiers = {}
+    for line in data_to_list:
+        split_line = line.split("\t")
+        if len(split_line) > 1:
+            mac_identifiers[split_line[0].lower()] = split_line[1]
+    return mac_identifiers
+
 def arp_request(ip_address):
     arp_packet = scapy.ARP(pdst=ip_address)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     full_packet = broadcast / arp_packet
     answered = scapy.srp(full_packet, timeout=1, iface=config.ntwrk_adapter, verbose=False)[0]
+    mac_identifiers = collect_vendor_mac()
     
+    print("--------------------------------------------------------------\nIP\t\t\tMAC\t\t\tVendor\n--------------------------------------------------------------")
     for host in answered:
-        print(host[1].psrc)
-        print(host[1].hwsrc)
-        print("--------------------------------------------------------------")
+        current_mac = host[1].hwsrc.replace(':', '').lower()
+        oui = current_mac[0:6]
+        print(oui)
+        vendor = mac_identifiers.get(oui, "UNKNOWN")
+        print(host[1].psrc + "\t\t" + host[1].hwsrc + "\t" + vendor)
 
 def device_info(host):
     try:
